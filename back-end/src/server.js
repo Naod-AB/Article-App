39761731087b61783f5737270f7dfaa0016cedce
Naod <naodat.dev@gmail.com>
2024-02-09 12:05:33 +0300

    refactored

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,10 +1,9 @@
 import express from 'express';
 
 const app = express();
-app.use(express.json());
 
-app.post('/hello', (req, res) => {
-    res.send(`Hello ${req.body.name}!`);
+app.get('/hello', (req, res) => {
+    res.send('Hello!');
 });
 
 app.listen(8000, () => {

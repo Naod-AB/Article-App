b49a12f12135490cf83d7e0ee24e3edf9bf6de2c
Naod <naodat.dev@gmail.com>
2024-02-16 14:51:14 +0300

    App Finished and deplyed online

--- back-end/src/db.js
+++ back-end/src/db.js
@@ -3,11 +3,12 @@ import { MongoClient } from 'mongodb';
 let db;
 
 async function connectToDb(cb) {
-    const client = new MongoClient('mongodb://127.0.0.1:27017');
+    const client = new MongoClient(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.myrvehk.mongodb.net/`);
     await client.connect();
-    db = client.db('react-blog-db');
+    db = client.db('react-blog-app');
     cb();
 }
+
 
 export {
     db,
